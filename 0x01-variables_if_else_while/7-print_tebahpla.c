3edd9e0adab850737b867dfcc3f006a1594f42b9
lharbeeb <lharbeeb@gmail.com>
2022-09-11 21:08:55 -0700

    subsequent commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[26] = "zyxwvutsrqponmlkjihgfedcba"
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
